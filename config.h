68f7dc7a142191ca6ecdd0e2d7cc646eb49284b1
Klaus Birkelund Jensen <birkelund@nbi.ku.dk>
2012-06-14 00:12:28 +0200

    Added layout cycling.

--- config.h
+++ config.h
@@ -1,19 +1,20 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]            = "-*-terminus-medium-r-*-*-16-*-*-*-*-*-*-*";
-#define NUMCOLORS 9
-static const char colors[NUMCOLORS][ColLast][9] = {
+static const char font[]            = "-*-terminus-medium-r-*-*-12-*-*-*-*-*-*-*";
+#define NUMCOLORS 10
+static const char colors[NUMCOLORS][ColLast][8] = {
 // border foreground background
-{ "#212121", "#696969", "#121212" }, // 0 = normal
-{ "#696969", "#E0E0E0", "#121212" }, // 1 = selected
-{ "#212121", "#CF4F88", "#121212" }, // 2 = red
-{ "#212121", "#53A6A6", "#121212" }, // 3 = green
-{ "#212121", "#914E89", "#121212" }, // 4 = yellow
-{ "#212121", "#4779B3", "#121212" }, // 5 = blue
-{ "#212121", "#47959E", "#121212" }, // 6 = cyan
-{ "#212121", "#7E62B3", "#121212" }, // 7 = magenta
-{ "#212121", "#899CA1", "#121212" }, // 8 = grey
+{ "#212121", "#696969", "#121212" }, // A  1 = normal
+{ "#696969", "#E0E0E0", "#121212" }, // B  2 = selected
+{ "#212121", "#DC322F", "#121212" }, // C  3 = urgent (red)
+{ "#212121", "#719E07", "#121212" }, // D  4 = green
+{ "#212121", "#B58900", "#121212" }, // E  5 = yellow
+{ "#212121", "#268BD2", "#121212" }, // F  6 = blue
+{ "#212121", "#2AA198", "#121212" }, // G  7 = cyan
+{ "#212121", "#D33682", "#121212" }, // H  8 = magenta
+{ "#212121", "#899CA1", "#121212" }, // I  9 = grey
+{ "#212121", "#CB4B16", "#121212" }, // J 10 = orange
 };
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -43,12 +44,13 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-	{ "TTT",      bstack },
-	{ "===",      bstackhoriz },
+	{ "[||]",     bstack },
+	{ "[=]",      bstackhoriz },
+	{ NULL,       NULL },
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -59,8 +61,8 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", colors[0][ColBG], "-nf", colors[0][ColFG], "-sb", colors[1][ColBG], "-sf", colors[1][ColFG], NULL };
-static const char *termcmd[]  = { "uxterm", NULL };
+static const char  *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", colors[0][ColBG], "-nf", colors[0][ColFG], "-sb", colors[1][ColBG], "-sf", colors[1][ColFG], NULL };
+static const char *termcmd[]  = { "urxvtc", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -79,8 +81,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_space,  nextlayout,     {0} },
+	{ MODKEY|ShiftMask,             XK_space,  prevlayout,     {0} },
+	{ MODKEY|ShiftMask,             XK_f,      togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
